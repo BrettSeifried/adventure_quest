39073262c958b7649dfeeb5141522018c7df0638
Brett Seifried <seifriedbrett@gmail.com>
2021-10-26 13:49:11 -0700

    functions created. quests.js has is now showing on quest page

--- utils.js
+++ utils.js
@@ -1,14 +1,13 @@
-// const user = { 
-//     completed: {
-//         dragon: true, 
-//         treasure: true,
-//         },
-//     gold: 130,
-//     hp: 35,
-//     name: "dani",
-//     race: "cleric",
-// }
+import quests from './data/quest-data.js';
 
+export function findById(items, id){
+    for (let item of items){
+        if (item.id === id){
+            return item;
+        }
+    }
+}
+        
 export function generateUser(formData){
     return {
         completed: {},
@@ -22,4 +21,24 @@ export function generateUser(formData){
 export function setUser(userObject){
     const userString = JSON.stringify(userObject);
     localStorage.setItem('USER', userString);
+}
+
+export function getUser(){
+    const userString = localStorage.getItem('USER');
+    return JSON.parse(userString);
+}
+
+export function scoreQuest(choiceOBject, questId, userObject){
+    userObject.hp += choiceOBject.hp;
+    userObject.gold += choiceOBject.gold;
+    userObject.compelted[questId] = true;
+}
+
+export function hasCompletedAllQuests(userObject){
+    for (let quest of quests){
+        if (!userObject.completed[quest.id]){
+            return false;
+        }
+    }
+    return true;
 }
