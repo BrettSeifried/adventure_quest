ccc77ee49cf95bfbb274e1672c099f1a81787816
Brett Seifried <seifriedbrett@gmail.com>
2021-10-25 17:39:34 -0700

    attempted URLSearchParams end of day push

--- quests/quests.js
+++ quests/quests.js
@@ -3,8 +3,6 @@ console.log(window.location.search);
 const params = new URLSearchParams(window.location.search);
 
 const id = params.get('id');
-id.classList.add('h1');
 
-id.append(id);
-
-return id;
+const quest = document.getElementById('choices');
+quest.classList.add(id);
